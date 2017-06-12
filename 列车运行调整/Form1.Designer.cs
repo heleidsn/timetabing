1563663715ecab16f0169bf2ab2da76d78c8efbd
598269931@qq.com <598269931@qq.com>
2017-06-12 19:33:39 +0800

    添加源码

---
+++ 列车运行调整/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace 列车运行调整
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.读取运行图ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.晚点信息设置ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.执行调整ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.显示时刻表ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.读取运行图ToolStripMenuItem,
+            this.晚点信息设置ToolStripMenuItem,
+            this.执行调整ToolStripMenuItem,
+            this.显示时刻表ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1122, 25);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 读取运行图ToolStripMenuItem
+            // 
+            this.读取运行图ToolStripMenuItem.Name = "读取运行图ToolStripMenuItem";
+            this.读取运行图ToolStripMenuItem.Size = new System.Drawing.Size(80, 21);
+            this.读取运行图ToolStripMenuItem.Text = "读取运行图";
+            this.读取运行图ToolStripMenuItem.Click += new System.EventHandler(this.读取运行图ToolStripMenuItem_Click);
+            // 
+            // 晚点信息设置ToolStripMenuItem
+            // 
+            this.晚点信息设置ToolStripMenuItem.Name = "晚点信息设置ToolStripMenuItem";
+            this.晚点信息设置ToolStripMenuItem.Size = new System.Drawing.Size(92, 21);
+            this.晚点信息设置ToolStripMenuItem.Text = "晚点信息设置";
+            // 
+            // 执行调整ToolStripMenuItem
+            // 
+            this.执行调整ToolStripMenuItem.Name = "执行调整ToolStripMenuItem";
+            this.执行调整ToolStripMenuItem.Size = new System.Drawing.Size(68, 21);
+            this.执行调整ToolStripMenuItem.Text = "执行调整";
+            this.执行调整ToolStripMenuItem.Click += new System.EventHandler(this.执行调整ToolStripMenuItem_Click);
+            // 
+            // 显示时刻表ToolStripMenuItem
+            // 
+            this.显示时刻表ToolStripMenuItem.Name = "显示时刻表ToolStripMenuItem";
+            this.显示时刻表ToolStripMenuItem.Size = new System.Drawing.Size(80, 21);
+            this.显示时刻表ToolStripMenuItem.Text = "显示时刻表";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 25);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1122, 623);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1122, 648);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 读取运行图ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 晚点信息设置ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 执行调整ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 显示时刻表ToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ 列车运行调整/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 列车运行调整
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class PublicValue   //全局变量设置
+        {
+            public static int[,] T_plan = new int[14,10];
+            public int B;
+        }
+
+        private void 读取运行图ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //int[,] T_plan = new int[14,10];
+            //读取标准运行图数据
+            int[,] T_plan = {
+            {0,32,32,68,68,79,85,109,109,137},
+            {6,38,38,74,74,85,91,115,115,143},
+            {12,44,44,80,80,91,97,121,121,149},
+            {18,57,57,101,106,120,126,156,156,191},
+            {24,63,63,107,112,126,132,162,162,197},
+            {30,78,78,132,138,155,162,198,204,248},
+            {36,84,84,138,144,161,168,204,210,254},
+            {42,90,90,144,150,167,174,210,216,260},
+            {48,96,96,150,156,173,180,216,222,266},
+            {54,102,102,156,162,179,186,222,228,272},
+            {172,197,197,226,226,235,241,261,261,284},
+            {178,203,203,232,232,241,247,267,267,290},
+            {184,209,209,238,238,247,253,273,273,296},
+            {190,215,215,244,244,253,259,279,279,302}};
+            
+            
+
+            //变量定义
+            Point startP = new Point(100, 100);
+            Point endP = new Point(200, 200);
+
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            Graphics gr = Graphics.FromImage(pictureBox1.Image);
+            gr.Clear(Color.White);
+            Pen PenGreen = new Pen(Color.Green, 1);//绿 虚线 细
+            Pen PenBlack = new Pen(Color.Black, 3);
+            //gr.DrawLine(PenGreen, startP,endP);
+
+
+            //绘制框架
+            int stationNum = 6;
+            int[] Y = {0, 48, 54, 17, 36, 44 };
+            int[] lineY = new int[stationNum];
+            string[] stationName = { "车站1", "车站2", "车站3", "车站4", "车站5", "车站6" };
+            lineY[0] = startP.Y;
+            gr.DrawLine(PenGreen, 100, lineY[0], 1050, lineY[0]);
+            Font drawFont = new Font("Arial", 16);
+            SolidBrush drawBrush = new SolidBrush(Color.Black);
+
+            gr.DrawString(stationName[0], drawFont,drawBrush, new Point(20, lineY[0]-10));
+            for (int i = 1; i < stationNum; i++)
+            {
+                lineY[i] = lineY[i - 1] + Y[i] * 2;
+                gr.DrawLine(PenGreen,100,lineY[i],1050,lineY[i]);
+                gr.DrawString(stationName[i], drawFont, drawBrush, new Point(20, lineY[i] - 10));
+            }
+
+
+
+            //绘制标准运行图
+            for (int i = 0; i < 14; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    if (j % 2 == 0) //发车
+                    {
+                        Point p1 = new Point(startP.X + T_plan[i, j]*3, lineY[j / 2]);
+                        Point p2 = new Point(startP.X + T_plan[i, j + 1]*3, lineY[j / 2 + 1]); 
+                        gr.DrawLine(PenBlack,p1,p2);
+                    }
+                    else
+                    {
+                        Point p1 = new Point(startP.X + T_plan[i, j] * 3, lineY[j / 2 + 1]);
+                        Point p2 = new Point(startP.X + T_plan[i, j + 1] * 3, lineY[j / 2 + 1]);
+                        gr.DrawLine(PenBlack, p1, p2);
+                    }
+                }
+            }
+
+
+
+        }
+
+        private void 执行调整ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            PublicValue.A = 10;
+            test();
+        }
+
+        private void test()
+        {
+            PublicValue.A = 20;
+            Console.WriteLine(PublicValue.A);
+        }
+    }
+}

e6802acc869e8e336196cc6060bf071506f276e3
598269931@qq.com <598269931@qq.com>
2017-06-13 12:32:49 +0800

    增加晚点设置

---
+++ 列车运行调整/DelayInfo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 列车运行调整
+{
+    public partial class DelayInfo : Form
+    {
+        public DelayInfo()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //TODO 检测输入是否合法
+            Form1.PublicValue.delayTrainNo = int.Parse(textBox1.Text);
+            Form1.PublicValue.delayBlock = int.Parse(textBox2.Text);
+            Form1.PublicValue.delayTime = int.Parse(textBox3.Text);
+            MessageBox.Show("设置成功");
+            this.Close();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}

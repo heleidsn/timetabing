04128ec0cbd2e5e17c946747f1a12f203bfbaaa7
heleidsn <598269931@qq.com>
2017-11-27 11:36:20 +0800

    增加Form2

--- 列车运行调整/Program.cs
+++ 列车运行调整/Program.cs
@@ -16,7 +16,7 @@ namespace 列车运行调整
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form2());
         }
     }
 }
